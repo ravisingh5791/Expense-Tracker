c55f7bbd77f63d7786c815ff75cdbe78133f27eb
Ravi singh <singhravi7313@gmail.com>
2026-03-28 00:46:03 +0530

    Update API_BASE_URL to use environment variable
    update API base

--- frontend/src/services/auth.config.js
+++ frontend/src/services/auth.config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = "http://localhost:8080/mywallet";
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/mywallet";
 
 export default API_BASE_URL;
